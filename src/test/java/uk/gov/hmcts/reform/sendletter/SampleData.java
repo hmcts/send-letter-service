130dfae7e5726f6fe7bd7b9730e81acfae8643c9
Alex McAusland <alex.mcausland@gmail.com>
2018-03-23 16:24:23 +0000

    Merge master

--- src/test/java/uk/gov/hmcts/reform/sendletter/SampleData.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/SampleData.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.reform.sendletter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.io.Resources;
-import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 import uk.gov.hmcts.reform.sendletter.model.in.Document;
 import uk.gov.hmcts.reform.sendletter.model.in.Letter;
 
