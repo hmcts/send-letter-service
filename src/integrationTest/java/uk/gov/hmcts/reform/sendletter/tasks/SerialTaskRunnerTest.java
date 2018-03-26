c92c652b203ea8170501413a143efc7bebc35b00
Alex McAusland <alex.mcausland@gmail.com>
2018-03-26 14:54:03 +0100

    Fix name of SerialTaskRunner test

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/tasks/SerialTaskRunnerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/tasks/SerialTaskRunnerTest.java
@@ -22,7 +22,7 @@ public class SerialTaskRunnerTest {
     @Autowired DataSource source;
 
     @Test
-    public void runs_task() throws SQLException {
+    public void runs_task_if_same_task_not_already_running() throws SQLException {
         Runnable runnable = mock(Runnable.class);
         SerialTaskRunner.get(source).tryRun(1, runnable);
         verify(runnable).run();
