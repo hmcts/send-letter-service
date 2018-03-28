3c0ae6502b36382eb95c58b73d11621f94501615
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-28 10:44:43 +0100

    Rename ftp exception

--- src/main/java/uk/gov/hmcts/reform/sendletter/exception/FtpStepException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.sendletter.exception;
-
-public class FtpStepException extends RuntimeException {
-    public FtpStepException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
