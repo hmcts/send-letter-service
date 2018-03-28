1ca7e3273cf9c7f73d1870d108a35bfb7a242639
Alex McAusland <alex.mcausland@gmail.com>
2018-03-28 09:48:57 +0100

    Use FileNameHelper to write Xerox reports

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/util/XeroxReportWriter.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/util/XeroxReportWriter.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.sendletter.util;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.assertj.core.util.Lists;
+import uk.gov.hmcts.reform.slc.services.steps.getpdf.FileNameHelper;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -15,7 +16,7 @@ import java.util.stream.Stream;
 public final class XeroxReportWriter {
     public static void writeReport(Stream<UUID> letterIds, File reportFolder) throws IOException {
         // We expect these fields in Xerox reports
-        CSVFormat csvFileFormat = CSVFormat.EXCEL.withHeader(
+        CSVFormat csvFileFormat = CSVFormat.DEFAULT.withHeader(
             "Date",
             "Time",
             "Filename"
@@ -25,8 +26,7 @@ public final class XeroxReportWriter {
         List<List<String>> records = letterIds.map(x -> Lists.newArrayList(
             "2018-01-01",
             "10:30:53",
-            // Id is the third underscore separated part of filename.
-            String.format("a_b_%s", x)
+            FileNameHelper.generateName("aType", "aService", x, "pdf")
         )).collect(Collectors.toList());
 
         File report = new File(reportFolder, UUID.randomUUID() + ".csv");
