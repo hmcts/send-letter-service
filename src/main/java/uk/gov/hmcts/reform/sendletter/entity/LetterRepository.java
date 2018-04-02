b5b3ea28da781e8cf5265a538780d83741f2c57d
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-04-02 11:02:48 +0100

    Introduce stale letter task

--- src/main/java/uk/gov/hmcts/reform/sendletter/entity/LetterRepository.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/entity/LetterRepository.java
@@ -2,17 +2,28 @@ package uk.gov.hmcts.reform.sendletter.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Timestamp;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 import javax.persistence.LockModeType;
 
 public interface LetterRepository extends JpaRepository<Letter, UUID> {
+
+    String STALE_LETTERS_QUERY = "select l "
+        + "from Letter l "
+        + "where l.status = ?1"
+        + "  and l.sentToPrintAt < ?2";
+
     // This lockmode locks the returned rows
     // for both reading and writing.
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Stream<Letter> findByStatus(LetterStatus status);
+
+    @Query(value = STALE_LETTERS_QUERY)
+    Stream<StaleLetter> findStaleLetters(LetterStatus state, Timestamp before);
 
     Optional<Letter> findByMessageIdAndStatusOrderByCreatedAtDesc(String messageId, LetterStatus status);
 
--- src/main/java/uk/gov/hmcts/reform/sendletter/logging/AppInsights.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/logging/AppInsights.java
@@ -4,9 +4,16 @@ import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.telemetry.Duration;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.logging.appinsights.AbstractAppInsights;
+import uk.gov.hmcts.reform.sendletter.entity.StaleLetter;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class AppInsights extends AbstractAppInsights {
+
+    static final String LETTER_NOT_PRINTED = "LetterNotPrinted";
 
     public AppInsights(TelemetryClient telemetry) {
         super(telemetry);
@@ -19,6 +26,20 @@ public class AppInsights extends AbstractAppInsights {
             new Duration(duration.toMillis()),
             success
         );
+    }
+
+    public void trackStaleLetter(StaleLetter staleLetter) {
+        LocalDateTime sentToPrint = staleLetter.getSentToPrintAt().toLocalDateTime();
+        Map<String, String> properties = new HashMap<>();
+
+        properties.put("letterId", staleLetter.getId().toString());
+        properties.put("messageId", staleLetter.getMessageId());
+        properties.put("service", staleLetter.getService());
+        properties.put("type", staleLetter.getType());
+        properties.put("weekday", sentToPrint.getDayOfWeek().name());
+        properties.put("sentToPrintAt", sentToPrint.toLocalTime().toString());
+
+        telemetry.trackEvent(LETTER_NOT_PRINTED, properties, null);
     }
 
     public void trackException(Exception exception) {
