26933325b2ab1db657540ca4e1b4feb192b7a263
Rafal Kondratowicz <rkondr@gmail.com>
2018-04-11 11:50:46 +0200

    Add smoke test

--- src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/SmokeTestSuite.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/SmokeTestSuite.java
@@ -1,13 +1,68 @@
 package uk.gov.hmcts.reform.sendletter.controllers;
 
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Resources;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RunWith(SpringRunner.class)
 @SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
+@TestPropertySource("classpath:application.properties")
 public abstract class SmokeTestSuite {
 
-    @Value("${test-url:http://localhost:8485}")
+    @Value("${test-url}")
     protected String testUrl;
+
+    @Value("${s2s-url}")
+    protected String s2sUrl;
+
+    @Value("${s2s-name}")
+    protected String s2sName;
+
+    @Value("${s2s-secret}")
+    protected String s2sSecret;
+
+    /**
+     * Sign in to s2s.
+     *
+     * @return s2s JWT token.
+     */
+    protected String signIn() {
+        Map<String, Object> params = ImmutableMap.of(
+            "microservice", this.s2sName,
+            "oneTimePassword", new GoogleAuthenticator().getTotpPassword(this.s2sSecret)
+        );
+
+        Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(this.s2sUrl)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .body(params)
+            .post("/lease")
+            .andReturn();
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+
+        return response
+            .getBody()
+            .print();
+    }
+
+    protected String sampleLetterJson(String fileName) throws IOException {
+        return Resources.toString(Resources.getResource(fileName), Charsets.UTF_8);
+    }
 }
