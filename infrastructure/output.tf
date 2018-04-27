97dc4d61e91e5791b001b6ac66704efd089011bb
nitinprabhu <prabhunitin61@gmail.com>
2018-04-27 13:58:17 +0100

    Added vault name and vault uri

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,9 +1,9 @@
 output "vaultUri" {
-  value = "${module.key-vault.key_vault_uri}"
+  value = "${local.vaultUri}"
 }
 
 output "vaultName" {
-  value = "${module.key-vault.key_vault_name}"
+  value = "${local.vaultName}"
 }
 
 output "microserviceName" {
