37cce982b37f5da9dfdec74d324eff078041911a
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-09-06 14:03:04 +0100

    Remove pool as it wasn't there before either

--- src/main/java/uk/gov/hmcts/reform/sendletter/config/ThreadPoolConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/config/ThreadPoolConfig.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sendletter.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -21,12 +20,9 @@ public class ThreadPoolConfig {
     }
 
     @Bean
-    public ThreadPoolTaskScheduler threadPoolTaskScheduler(
-        @Value("${scheduling.pool}") int poolSize
-    ) {
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
 
-        threadPoolTaskScheduler.setPoolSize(poolSize);
         threadPoolTaskScheduler.setThreadNamePrefix("SendLetterTask-");
         threadPoolTaskScheduler.setErrorHandler(t -> {
             log.error("Unhandled exception during task. {}: {}", t.getClass(), t.getMessage(), t);
