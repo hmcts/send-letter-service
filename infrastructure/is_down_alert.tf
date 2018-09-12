9fc506ef3abd066a382d4612c5649f25a62c912f
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-09-12 13:47:41 +0100

    Make use of multiline string assignment for better reading

--- infrastructure/is_down_alert.tf
+++ infrastructure/is_down_alert.tf
@@ -5,7 +5,20 @@ module "send-letter-up-alert" {
 
   alert_name = "Send Letter is DOWN - RPE"
   alert_desc = "Triggers when send letter service looks like being down within a 15 minutes timeframe."
-  app_insights_query = "union (dependencies | where data == 'FtpFileUploaded' and success =~ 'true' | summarize ['name'] = 'uploaded', ['counter'] = sum(itemCount)), (requests | where name == 'POST SendLetterController/sendLetter' and success =~ 'true' | where tostring(operation_SyntheticSource) == '' | summarize ['name'] = 'received', ['counter'] = sum(itemCount)), (traces | extend logger = customDimensions.LoggerName | where logger == 'uk.gov.hmcts.reform.sendletter.tasks.UploadLettersTask' and message has 'FTP downtime' | summarize ['name'] = 'downtime', ['counter'] = sum(itemCount)) | evaluate pivot(name, sum(counter)) | project ['is_operational'] = iif(received > 0 and downtime > 0, true, iif(received > 0, uploaded > 0, true)) | where is_operational == false"
+  app_insights_query = <<EOF
+union (dependencies
+ | where data == 'FtpFileUploaded' and success =~ 'true'
+ | summarize ['name'] = 'uploaded', ['counter'] = sum(itemCount)), (requests
+ | where name == 'POST SendLetterController/sendLetter' and success =~ 'true'
+ | where tostring(operation_SyntheticSource) == ''
+ | summarize ['name'] = 'received', ['counter'] = sum(itemCount)), (traces
+ | extend logger = customDimensions.LoggerName
+ | where logger == 'uk.gov.hmcts.reform.sendletter.tasks.UploadLettersTask' and message has 'FTP downtime'
+ | summarize ['name'] = 'downtime', ['counter'] = sum(itemCount))
+ | evaluate pivot(name, sum(counter))
+ | project ['is_operational'] = iif(received > 0 and downtime > 0, true, iif(received > 0, uploaded > 0, true))
+ | where is_operational == false
+EOF
   frequency_in_minutes = 10
   time_window_in_minutes = 15
   severity_level = "2"
