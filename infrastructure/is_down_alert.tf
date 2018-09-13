80bab7dd1b76606eecc4afb4659eb90ae34a8129
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-09-13 09:21:12 +0100

    Apply suggested formatting

--- infrastructure/is_down_alert.tf
+++ infrastructure/is_down_alert.tf
@@ -7,18 +7,24 @@ module "send-letter-up-alert" {
   alert_desc = "Triggers when send letter service looks like being down within a 15 minutes timeframe."
 
   app_insights_query = <<EOF
-union (dependencies
- | where data == 'FtpFileUploaded' and success =~ 'true'
- | summarize ['name'] = 'uploaded', ['counter'] = sum(itemCount)), (requests
- | where name == 'POST SendLetterController/sendLetter' and success =~ 'true'
- | where tostring(operation_SyntheticSource) == ''
- | summarize ['name'] = 'received', ['counter'] = sum(itemCount)), (traces
- | extend logger = customDimensions.LoggerName
- | where logger == 'uk.gov.hmcts.reform.sendletter.tasks.UploadLettersTask' and message has 'FTP downtime'
- | summarize ['name'] = 'downtime', ['counter'] = sum(itemCount))
- | evaluate pivot(name, sum(counter))
- | project ['is_operational'] = iif(received > 0 and downtime > 0, true, iif(received > 0, uploaded > 0, true))
- | where is_operational == false
+union
+  (dependencies
+    | where data == 'FtpFileUploaded' and success =~ 'true'
+    | summarize ['name'] = 'uploaded', ['counter'] = sum(itemCount)
+  ),
+  (requests
+    | where name == 'POST SendLetterController/sendLetter' and success =~ 'true'
+    | where tostring(operation_SyntheticSource) == ''
+    | summarize ['name'] = 'received', ['counter'] = sum(itemCount)
+  ),
+  (traces
+    | extend logger = customDimensions.LoggerName
+    | where logger == 'uk.gov.hmcts.reform.sendletter.tasks.UploadLettersTask' and message has 'FTP downtime'
+    | summarize ['name'] = 'downtime', ['counter'] = sum(itemCount)
+  )
+| evaluate pivot(name, sum(counter))
+| project ['is_operational'] = iif(received > 0 and downtime > 0, true, iif(received > 0, uploaded > 0, true))
+| where is_operational == false
 EOF
 
   frequency_in_minutes       = 10
