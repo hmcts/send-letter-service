ae60e6f97cd87372510366272cb5fab2f1761dbe
Aliveni Choppa <aliveni.ch@gmail.com>
2018-12-05 14:11:59 +0000

    removed unused import

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/ftp/FtpClient.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/ftp/FtpClient.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
