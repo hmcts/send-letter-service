844cb5c419f1237fed20c98251645226c7fb05a0
Rafal Kondratowicz <rkondr@gmail.com>
2018-12-18 15:01:55 +0000

    Remove unused import

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/services/LetterServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/services/LetterServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.pdf.generator.HTMLToPDFConverter;
 import uk.gov.hmcts.reform.sendletter.PdfHelper;
