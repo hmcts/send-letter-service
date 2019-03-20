af0051f54d8b58683401b3e8c0a94752a6b41fc0
Donatas Martinkus <donatas.martinkus@hmcts.net>
2019-03-20 14:58:47 +0000

    Revert "Fix ftp properties for e2e tests"
    This reverts commit 79ae077eb48c2968b7ee252e7a30639f4af51b54.

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/e2e/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/e2e/BaseTest.java
@@ -5,17 +5,14 @@ import com.google.common.io.Resources;
 import org.bouncycastle.openpgp.PGPException;
 import org.junit.After;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import uk.gov.hmcts.reform.sendletter.PdfHelper;
-import uk.gov.hmcts.reform.sendletter.config.FtpConfigProperties;
 import uk.gov.hmcts.reform.sendletter.config.ThreadPoolConfig;
 import uk.gov.hmcts.reform.sendletter.entity.Letter;
 import uk.gov.hmcts.reform.sendletter.entity.LetterRepository;
@@ -44,7 +41,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration
 @SpringBootTest
 @DirtiesContext
-@Import(BaseTest.TestConfiguration.class)
 public class BaseTest {
 
     @Autowired
@@ -153,13 +149,5 @@ public class BaseTest {
             return false;
         }
         return true;
-    }
-
-    @EnableConfigurationProperties(FtpConfigProperties.class)
-    static class TestConfiguration {
-        private TestConfiguration(FtpConfigProperties properties) throws IOException {
-            properties.setPublicKey(Resources.toString(Resources.getResource("keypair.pub"), Charsets.UTF_8));
-            properties.setPrivateKey(Resources.toString(Resources.getResource("keypair"), Charsets.UTF_8));
-        }
     }
 }
