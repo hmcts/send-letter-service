3111738e4b2df1593b723fcc28cc3d6a627c0733
Donatas Martinkus <doncem@users.noreply.github.com>
2019-04-24 14:24:36 +0100

    Provide configuration for `RestTemplate`

---
+++ src/main/java/uk/gov/hmcts/reform/sendletter/config/RestTemplateConfiguration.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.sendletter.config;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Autowired
+    private CloseableHttpClient httpClient;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        clientHttpRequestFactory.setHttpClient(httpClient);
+
+        return clientHttpRequestFactory;
+    }
+}
