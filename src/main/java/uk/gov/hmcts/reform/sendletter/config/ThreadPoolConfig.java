4a306397185d4ef1bb56c71f19d51137fe0b6f7f
Donatas Martinkus <doncem@users.noreply.github.com>
2019-04-30 19:43:57 +0100

    Move `@EnableScheduling` to the rightful place

--- src/main/java/uk/gov/hmcts/reform/sendletter/config/ThreadPoolConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/config/ThreadPoolConfig.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * Adds custom error handler to Scheduled Tasks.
  */
 @Configuration
+@EnableScheduling
 public class ThreadPoolConfig {
 
     private static int errorCount;
