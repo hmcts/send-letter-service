2bdf70f679d4438f73a83f67e4ace1c8860727ef
Aliveni Choppa <aliveni.ch@gmail.com>
2019-04-30 19:21:59 +0100

    Updated repository to use LocalDateTime for date params. Updated tests to format the dates with ZoneId

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/entity/LettersCountSummaryRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/entity/LettersCountSummaryRepositoryTest.java
@@ -9,14 +9,14 @@ import uk.gov.hmcts.reform.sendletter.SampleData;
 import uk.gov.hmcts.reform.sendletter.entity.reports.ServiceLettersCount;
 import uk.gov.hmcts.reform.sendletter.entity.reports.ServiceLettersCountSummary;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.sendletter.util.TimeZones.EUROPE_LONDON;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
@@ -26,7 +26,7 @@ public class LettersCountSummaryRepositoryTest {
     private LettersCountSummaryRepository repository;
 
     @Test
-    void should_get_letters_count_group_by_service() throws ParseException {
+    void should_get_letters_count_group_by_service() {
         //given
         LocalDateTime date = LocalDateTime.parse("2019-04-24T10:15:30");
         repository.save(letterEntity("a.service", date.minusHours(1)));
@@ -38,10 +38,10 @@ public class LettersCountSummaryRepositoryTest {
         assertThat(letters.size()).isEqualTo(4);
 
         //when
-        Date dateFrom = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse("2019-04-23 17:00");
-        Date dateTo = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse("2019-04-24 16:00");
-
-        List<ServiceLettersCountSummary> result = repository.countByDate(dateFrom, dateTo);
+        List<ServiceLettersCountSummary> result = repository.countByDate(
+            LocalDateTime.parse("2019-04-23T17:00:00"),
+            LocalDateTime.parse("2019-04-24T16:00:00")
+        );
 
         //then
         assertThat(result)
@@ -57,9 +57,9 @@ public class LettersCountSummaryRepositoryTest {
     }
 
     @Test
-    void should_return_empty_when_no_letters_uploaded_for_the_requested_date() throws ParseException {
+    void should_return_empty_when_no_letters_uploaded_for_the_requested_date() {
         //given
-        LocalDateTime date = LocalDateTime.parse("2019-04-24T14:00:00");
+        LocalDateTime date = formatDateTimeWithZoneId("2019-04-24T14:00:00");
         repository.save(letterEntity("a.service", date.minusDays(1))); //previous day
         repository.save(letterEntity("b.service", date.minusDays(2)));
 
@@ -67,10 +67,10 @@ public class LettersCountSummaryRepositoryTest {
         assertThat(letters.size()).isEqualTo(2);
 
         //when
-        Date dateFrom = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse("2019-04-24 17:00");
-        Date dateTo = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse("2019-04-25 16:00");
-
-        List<ServiceLettersCountSummary> result = repository.countByDate(dateFrom, dateTo);
+        List<ServiceLettersCountSummary> result = repository.countByDate(
+            formatDateTimeWithZoneId("2019-04-23T17:00:00"),
+            formatDateTimeWithZoneId("2019-04-24T16:00:00")
+        );
 
         //then
         assertThat(result).isEmpty();
@@ -80,5 +80,12 @@ public class LettersCountSummaryRepositoryTest {
         Letter letter = SampleData.letterEntity(service);
         letter.setSentToPrintAt(sentToPrintAt);
         return letter;
+    }
+
+    private LocalDateTime formatDateTimeWithZoneId(String dateString) {
+        return LocalDateTime.parse(
+            dateString,
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").withZone(ZoneId.of(EUROPE_LONDON))
+        );
     }
 }
--- src/main/java/uk/gov/hmcts/reform/sendletter/entity/LettersCountSummaryRepository.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/entity/LettersCountSummaryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import uk.gov.hmcts.reform.sendletter.entity.reports.ServiceLettersCountSummary;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,14 +14,12 @@ public interface LettersCountSummaryRepository extends JpaRepository<Letter, UUI
     @Query(
         nativeQuery = true,
         value = "SELECT COUNT(1) as uploaded, service FROM letters\n"
-            + "  WHERE \n"
-            + "     sent_to_print_at >= :dateFrom\n"
-            + "         AND \n"
-            + "     sent_to_print_at <=  :dateTo\n"
+            + "  WHERE sent_to_print_at >= :dateFrom\n"
+            + "    AND sent_to_print_at <=  :dateTo\n"
             + "  GROUP BY service \n"
             + "  ORDER BY service ASC"
     )
     List<ServiceLettersCountSummary> countByDate(
-        @Param("dateFrom") Date dateFrom,
-        @Param("dateTo") Date dateTo);
+        @Param("dateFrom") LocalDateTime dateFrom,
+        @Param("dateTo") LocalDateTime dateTo);
 }
