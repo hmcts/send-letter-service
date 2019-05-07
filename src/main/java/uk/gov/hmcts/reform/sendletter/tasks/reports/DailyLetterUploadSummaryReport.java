a10a03a28e0b0b6bc65b300b3dde87d61748d2f0
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-07 13:34:35 +0100

    Escalating to `error` level

--- src/main/java/uk/gov/hmcts/reform/sendletter/tasks/reports/DailyLetterUploadSummaryReport.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/tasks/reports/DailyLetterUploadSummaryReport.java
@@ -56,7 +56,7 @@ public class DailyLetterUploadSummaryReport {
     @Scheduled(cron = "${reports.upload-summary.cron}", zone = EUROPE_LONDON)
     public void send() {
         if (recipients.length == 0) {
-            log.warn("No recipients configured for reports");
+            log.error("No recipients configured for reports");
             return;
         }
 
