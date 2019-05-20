83196965b92097faf66ecc3144f33ad09e61c24e
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-20 13:42:11 +0100

    Add test with non empty list

--- src/test/java/uk/gov/hmcts/reform/sendletter/services/PendingLettersServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/services/PendingLettersServiceTest.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.sendletter.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.sendletter.SampleData;
 import uk.gov.hmcts.reform.sendletter.entity.Letter;
 import uk.gov.hmcts.reform.sendletter.entity.LetterRepository;
 import uk.gov.hmcts.reform.sendletter.entity.LetterStatus;
@@ -12,16 +17,24 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class PendingLettersServiceTest {
+
+    @Mock
+    private LetterRepository repo;
+
+    private PendingLettersService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new PendingLettersService(repo);
+    }
 
     @Test
     void should_read_letters_in_proper_status_from_repo() {
         // given
-        LetterRepository repo = mock(LetterRepository.class);
-        PendingLettersService service = new PendingLettersService(repo);
         List<Letter> noLetters = Collections.emptyList();
         given(repo.findByStatus(any())).willReturn(noLetters);
 
@@ -34,5 +47,20 @@ class PendingLettersServiceTest {
         assertThat(letters).isEqualTo(noLetters);
         verify(repo).findByStatus(statusArgumentCaptor.capture());
         assertThat(statusArgumentCaptor.getValue()).isEqualTo(LetterStatus.Created);
+    }
+
+    @Test
+    void should_return_list_of_letters_from_repo() {
+        // given
+        List<Letter> letters = Collections.singletonList(SampleData.letterEntity("some service"));
+        given(repo.findByStatus(any())).willReturn(letters);
+
+        // when
+        List<Letter> lettersFromDb = service.getPendingLetters();
+
+        // then
+        assertThat(lettersFromDb)
+            .usingRecursiveFieldByFieldElementComparator()
+            .isEqualTo(letters);
     }
 }
