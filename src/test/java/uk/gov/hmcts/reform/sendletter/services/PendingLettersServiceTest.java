5462b0eb35c81b6238bf4dd9e39cfcc0e89eaa60
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-20 13:00:24 +0100

    Verify same list is returned

--- src/test/java/uk/gov/hmcts/reform/sendletter/services/PendingLettersServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/services/PendingLettersServiceTest.java
@@ -2,10 +2,16 @@ package uk.gov.hmcts.reform.sendletter.services;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import uk.gov.hmcts.reform.sendletter.entity.Letter;
 import uk.gov.hmcts.reform.sendletter.entity.LetterRepository;
 import uk.gov.hmcts.reform.sendletter.entity.LetterStatus;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -16,13 +22,16 @@ class PendingLettersServiceTest {
         // given
         LetterRepository repo = mock(LetterRepository.class);
         PendingLettersService service = new PendingLettersService(repo);
+        List<Letter> noLetters = Collections.emptyList();
+        given(repo.findByStatus(any())).willReturn(noLetters);
 
         ArgumentCaptor<LetterStatus> statusArgumentCaptor = ArgumentCaptor.forClass(LetterStatus.class);
 
         // when
-        service.getPendingLetters();
+        List<Letter> letters = service.getPendingLetters();
 
         // then
+        assertThat(letters).isEqualTo(noLetters);
         verify(repo).findByStatus(statusArgumentCaptor.capture());
         assertThat(statusArgumentCaptor.getValue()).isEqualTo(LetterStatus.Created);
     }
