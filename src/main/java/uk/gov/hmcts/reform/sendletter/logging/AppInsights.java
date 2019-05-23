aa8686a4af308d82f2c166e59eb1150596241a90
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-23 12:53:29 +0100

    Move aspect to app insights component and verify with e2e

--- src/main/java/uk/gov/hmcts/reform/sendletter/logging/AppInsights.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/logging/AppInsights.java
@@ -5,6 +5,14 @@ import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.core.dependencies.apachecommons.lang3.BooleanUtils;
 import com.microsoft.applicationinsights.telemetry.Duration;
 import com.microsoft.applicationinsights.telemetry.RemoteDependencyTelemetry;
+import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
+import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
+import com.microsoft.applicationinsights.web.internal.ThreadContext;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sendletter.entity.Letter;
 import uk.gov.hmcts.reform.sendletter.model.ParsedReport;
@@ -13,8 +21,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import static uk.gov.hmcts.reform.sendletter.util.TimeZones.getCurrentEuropeLondonInstant;
+
+@Aspect
 @Component
 public class AppInsights {
+
+    private static final Logger log = LoggerFactory.getLogger(AppInsights.class);
 
     static final String FTP_TYPE = "FTP";
 
@@ -30,6 +43,59 @@ public class AppInsights {
 
     public AppInsights(TelemetryClient telemetry) {
         this.telemetry = telemetry;
+    }
+
+    // schedules
+
+    @Around("@annotation(org.springframework.scheduling.annotation.Scheduled)")
+    public void aroundSchedule(ProceedingJoinPoint joinPoint) throws Throwable {
+        RequestTelemetryContext requestTelemetry = ThreadContext.getRequestTelemetryContext();
+        boolean success = false;
+
+        try {
+            joinPoint.proceed();
+
+            success = true;
+        } finally {
+            handleRequestTelemetry(requestTelemetry, joinPoint.getTarget().getClass().getSimpleName(), success);
+        }
+    }
+
+    private void handleRequestTelemetry(
+        RequestTelemetryContext requestTelemetryContext,
+        String caller,
+        boolean success
+    ) {
+        String requestName = "Schedule /" + caller;
+
+        if (requestTelemetryContext != null) {
+            handleRequestTelemetry(
+                requestTelemetryContext.getHttpRequestTelemetry(),
+                requestName,
+                requestTelemetryContext.getRequestStartTimeTicks(),
+                success
+            );
+        } else {
+            log.warn(
+                "Request Telemetry Context has been removed by ThreadContext - cannot log '{}' request",
+                requestName
+            );
+        }
+    }
+
+    private void handleRequestTelemetry(
+        RequestTelemetry requestTelemetry,
+        String requestName,
+        long start,
+        boolean success
+    ) {
+        if (requestTelemetry != null) {
+            requestTelemetry.setName(requestName);
+            requestTelemetry.setDuration(new Duration(getCurrentEuropeLondonInstant().toEpochMilli() - start));
+            requestTelemetry.setSuccess(success);
+
+            telemetry.trackRequest(requestTelemetry);
+        }
     }
 
     // dependencies
