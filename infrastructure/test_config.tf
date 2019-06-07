9a20f4ca5ef032cf0f1638398041aa885b219737
Mark Ryan <mark.ryan@hmcts.net>
2019-06-07 13:27:43 +0100

    get rid of secret resources

--- infrastructure/test_config.tf
+++ infrastructure/test_config.tf
@@ -4,21 +4,9 @@ data "azurerm_key_vault_secret" "source_test_s2s_secret" {
   vault_uri = "${local.s2s_vault_url}"
 }
 
-resource "azurerm_key_vault_secret" "test_s2s_secret" {
-  name      = "test-s2s-secret"
-  value     = "${data.azurerm_key_vault_secret.source_test_s2s_secret.value}"
-  vault_uri = "${module.send-letter-key-vault.key_vault_uri}"
-}
-
 data "azurerm_key_vault_secret" "source_test_ftp_user" {
   name      = "test-ftp-user"
   key_vault_id = "${module.send-letter-key-vault.key_vault_id}"
-}
-
-resource "azurerm_key_vault_secret" "test_ftp_user" {
-  name      = "test-ftp-user"
-  value     = "${data.azurerm_key_vault_secret.source_test_ftp_user.value}"
-  vault_uri = "${module.send-letter-key-vault.key_vault_uri}"
 }
 
 data "azurerm_key_vault_secret" "source_test_ftp_private_key" {
@@ -26,19 +14,7 @@ data "azurerm_key_vault_secret" "source_test_ftp_private_key" {
   key_vault_id = "${module.send-letter-key-vault.key_vault_id}"
 }
 
-resource "azurerm_key_vault_secret" "test_ftp_private_key" {
-  name      = "test-ftp-private-key"
-  value     = "${data.azurerm_key_vault_secret.source_test_ftp_private_key.value}"
-  vault_uri = "${module.send-letter-key-vault.key_vault_uri}"
-}
-
 data "azurerm_key_vault_secret" "source_test_ftp_public_key" {
   name      = "test-ftp-public-key"
   key_vault_id = "${module.send-letter-key-vault.key_vault_id}"
-}
-
-resource "azurerm_key_vault_secret" "test_ftp_public_key" {
-  name      = "test-ftp-public-key"
-  value     = "${data.azurerm_key_vault_secret.source_test_ftp_public_key.value}"
-  vault_uri = "${module.send-letter-key-vault.key_vault_uri}"
 }
