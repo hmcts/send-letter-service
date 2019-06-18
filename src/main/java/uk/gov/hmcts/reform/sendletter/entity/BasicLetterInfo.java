1c6b02e06eeadfcc713702c73af607b0cb1f6489
Rafal Kondratowicz <rkondr@gmail.com>
2019-06-18 13:59:04 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/sendletter/entity/BasicLetterInfo.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/entity/BasicLetterInfo.java
@@ -4,11 +4,18 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface BasicLetterInfo {
+
     UUID getId();
+
     String getChecksum();
+
     String getService();
+
     String getStatus();
+
     String getType();
+
     LocalDateTime getCreatedAt();
+
     LocalDateTime getSentToPrintAt();
 }
