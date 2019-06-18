1994a69f37da22abb3e78e3cfc397128c014938b
Rafal Kondratowicz <rkondr@gmail.com>
2019-06-18 13:29:48 +0100

    Don't load file contante for stale letters

---
+++ src/main/java/uk/gov/hmcts/reform/sendletter/entity/BasicLetterInfo.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.sendletter.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface BasicLetterInfo {
+    UUID getId();
+    String getChecksum();
+    String getService();
+    String getStatus();
+    String getType();
+    LocalDateTime getCreatedAt();
+    LocalDateTime getSentToPrintAt();
+}
