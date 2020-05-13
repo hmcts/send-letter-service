760e4ae1d7d397693d2ef02e64e581d9d083b6fe
Aliveni Choppa <aliveni.ch@gmail.com>
2020-05-13 13:47:34 +0100

    use correct db module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -141,7 +141,7 @@ data "azurerm_key_vault" "s2s_key_vault" {
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   key_vault_id = "${module.send-letter-key-vault.key_vault_id}"
   name         = "${var.component}-POSTGRES-USER"
-  value        = "${module.staging-db.user_name}"
+  value        = "${module.db.user_name}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
