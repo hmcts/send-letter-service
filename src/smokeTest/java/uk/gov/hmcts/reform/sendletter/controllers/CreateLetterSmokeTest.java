256efdbb79f0acd5c62dd28c0b4b46b7638cd536
Somesh Acharya <somesh.acharya@hmcts.net>
2020-08-17 15:43:30 +0100

    DTSBP-18: Parameterised the test

--- src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/CreateLetterSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/CreateLetterSmokeTest.java
@@ -2,24 +2,37 @@ package uk.gov.hmcts.reform.sendletter.controllers;
 
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.logging.appinsights.SyntheticHeaders;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 @ExtendWith(SpringExtension.class)
 class CreateLetterSmokeTest extends SmokeTestSuite {
 
-    @Test
-    void should_create_letter_successfully() throws Exception {
+    static Stream<Arguments> testData() {
+        return Stream.of(
+                arguments(MediaTypes.LETTER_V2, "letter-with-pdf.json"),
+                arguments(MediaTypes.LETTER_V3, "letter-with-document-count.json")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("testData")
+    void should_create_letter_successfully(String mediaType, String fileName) throws Exception {
 
         String jwt = signIn();
 
-        String id = givenJwt(jwt, MediaTypes.LETTER_V2)
+        String id = givenJwt(jwt, mediaType)
             .and()
-            .body(sampleLetterJson("letter-with-pdf.json"))
+            .body(sampleLetterJson(fileName))
             .when()
             .post("/letters")
             .then()
