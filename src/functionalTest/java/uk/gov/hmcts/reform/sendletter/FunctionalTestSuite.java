97017a5995254a889f4a8949240eb13ecde9c0e3
Somesh Acharya <somesh.acharya@hmcts.net>
2020-09-24 16:53:16 +0100

    DTSBP-84: Enabled relaxed https validation

--- src/functionalTest/java/uk/gov/hmcts/reform/sendletter/FunctionalTestSuite.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sendletter/FunctionalTestSuite.java
@@ -261,9 +261,11 @@ abstract class FunctionalTestSuite {
         String letterStatus = "Not found";
         while (letterStatus.equals("Not found") && counter <= 10) {
             try {
+                System.out.println(String.format(" TRYING TO FIND Letter id %s not found so, retrying %s", letterId, counter));
+                logger.accept(letterId, counter++);
                 letterStatus = getLetterStatus(letterId);
-            } catch (AssertionError e) {
-                System.out.println(String.format("Letter id %s not found so, retrying %s", letterId, counter));
+            } catch (RuntimeException e) {
+                System.out.println(String.format("ERROR Letter id %s not found so, retrying %s", letterId, counter));
                 logger.accept(letterId, counter++);
                 try {
                     Thread.sleep(100);
