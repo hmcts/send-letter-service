babda55a73db9373db4b2c64544f0598a44cc322
Jason Paige <jason.r.paige@gmail.com>
2020-11-26 13:41:36 +0000

    Remove unused pg9.6 RPE dbs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,31 +18,13 @@ locals {
   encryption_public_key = data.azurerm_key_vault_secret.encryption_public_key.value
 
   local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "aat" : "saat" : var.env
-  local_ase = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "core-compute-aat" : "core-compute-saat" : local.ase_name
 
-  s2s_rg  = "rpe-service-auth-provider-${local.local_env}"
-  s2s_url = "http://${local.s2s_rg}.service.core-compute-${local.local_env}.internal"
+  s2s_rg  = "rpe-service-auth-provider-${var.env}"
+  s2s_url = "http://${local.s2s_rg}.service.core-compute-${var.env}.internal"
 
   previewVaultName    = "${var.product}-send-letter"
   nonPreviewVaultName = "${var.product}-send-letter-${var.env}"
   vaultName           = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
-
-  db_connection_options = "?sslmode=require"
-
-  sku_size = var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"
-}
-
-module "db" {
-  source          = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product         = "${var.product}-${var.component}-db"
-  location        = var.location_db
-  env             = var.env
-  database_name   = "send_letter"
-  postgresql_user = "send_letter"
-  sku_name        = "GP_Gen5_2"
-  sku_tier        = "GeneralPurpose"
-  common_tags     = var.common_tags
-  subscription    = var.subscription
 }
 
 module "db-v11" {
@@ -92,7 +74,7 @@ module "send-letter-key-vault" {
 }
 
 data "azurerm_key_vault" "s2s_key_vault" {
-  name                = "s2s-${local.local_env}"
+  name                = "s2s-${var.env}"
   resource_group_name = local.s2s_rg
 }
 
