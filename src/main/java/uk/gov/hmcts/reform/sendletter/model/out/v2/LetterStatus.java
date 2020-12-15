8672dd720a8969c7993786271fad730a3bd4af6d
Somesh Acharya <somesh.acharya@hmcts.net>
2020-12-15 15:58:15 +0000

    DTSBPS-207: Storing number of copies in Json format.

--- src/main/java/uk/gov/hmcts/reform/sendletter/model/out/v2/LetterStatus.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/model/out/v2/LetterStatus.java
@@ -8,33 +8,7 @@ import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.UUID;
 
-public class LetterStatus {
-
-    public final UUID id;
-
-    public final String status;
-
-    @ApiModelProperty(notes = "This field is deprecated, please use `checksum` instead")
-    @JsonProperty("message_id")
-    public final String messageId;
-
-    @JsonProperty("checksum")
-    public final String checksum;
-
-    @JsonProperty("created_at")
-    public final ZonedDateTime createdAt;
-
-    @JsonProperty("sent_to_print_at")
-    public final ZonedDateTime sentToPrintAt;
-
-    @JsonProperty("printed_at")
-    public final ZonedDateTime printedAt;
-
-    @ApiModelProperty(value = "Additional information about the letter")
-    @JsonProperty("additional_data")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public final Map<String, Object> additionalData;
-
+public class LetterStatus extends uk.gov.hmcts.reform.sendletter.model.out.LetterStatus {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public final Map<String, Object> copies;
 
@@ -48,14 +22,7 @@ public class LetterStatus {
         final Map<String, Object> additionalData,
         final Map<String, Object> copies
     ) {
-        this.id = id;
-        this.status = status;
-        this.checksum = checksum;
-        this.messageId = checksum;
-        this.createdAt = createdAt;
-        this.sentToPrintAt = sentToPrintAt;
-        this.printedAt = printedAt;
-        this.additionalData = additionalData;
+        super(id, status, checksum, createdAt, sentToPrintAt, printedAt, additionalData, null);
         this.copies =  copies;
     }
 }
