aa4967ab8aab084714e1d4b4897e73dd8a9217bc
Jason Paige <j@jasonpaige.co.uk>
2021-04-19 14:37:27 +0100

    fix SA module output names

--- infrastructure/storage-account.tf
+++ infrastructure/storage-account.tf
@@ -11,7 +11,8 @@ locals {
     "backup",
     "processed",
     "zipped",
-  "encrypted"]
+    "encrypted"
+  ]
 
   valid_subnets = [
     data.azurerm_subnet.jenkins_subnet.id,
@@ -21,12 +22,13 @@ locals {
     data.azurerm_subnet.app_aks_01_subnet.id
   ]
 
+  short_component = replace(var.component, "-service", "")
 }
 
 module "storage_account" {
   source                   = "git@github.com:hmcts/cnp-module-storage-account.git?ref=mi-role-assignments"
   env                      = var.env
-  storage_account_name     = replace("${var.product}${var.component}${var.env}", "-", "")
+  storage_account_name     = replace("${var.product}${local.short_component}${var.env}", "-", "")
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_kind             = "StorageV2"
@@ -44,34 +46,34 @@ module "storage_account" {
 
 resource "azurerm_storage_container" "service_containers" {
   name                 = local.container_names[count.index]
-  storage_account_name = module.storage_account.name
+  storage_account_name = module.storage_account.storageaccount_name
   count                = length(local.container_names)
 }
 
 resource "azurerm_key_vault_secret" "storage_account_name" {
   name         = "storage-account-name"
-  value        = module.storage_account.name
+  value        = module.storage_account.storageaccount_name
   key_vault_id = module.send-letter-key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "storage_account_primary_key" {
   name         = "storage-account-primary-key"
-  value        = module.storage_account.primary_access_key
+  value        = module.storage_account.storageaccount_primary_access_key
   key_vault_id = module.send-letter-key-vault.key_vault_id
 }
 
 # this secret is used by blob-router-service for uploading blobs
 resource "azurerm_key_vault_secret" "storage_account_connection_string" {
   name         = "storage-account-connection-string"
-  value        = module.storage_account.primary_connection_string
+  value        = module.storage_account.storageaccount_primary_connection_string
   key_vault_id = module.send-letter-key-vault.key_vault_id
 }
 
 output "storage_account_name" {
-  value = module.storage_account.name
+  value = module.storage_account.storageaccount_name
 }
 
 output "storage_account_primary_key" {
   sensitive = true
-  value     = module.storage_account.primary_access_key
+  value     = module.storage_account.storageaccount_primary_access_key
 }
