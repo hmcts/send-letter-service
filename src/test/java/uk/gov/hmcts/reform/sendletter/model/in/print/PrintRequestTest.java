c8ee437fb5bbce6a64461280c72a564b3906265a
SomeshRA <somesh.acharya1@hmcts.net>
2021-04-29 16:22:11 +0100

    DTSBPS-427: Print job models

--- src/test/java/uk/gov/hmcts/reform/sendletter/model/in/print/PrintRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/model/in/print/PrintRequestTest.java
@@ -3,9 +3,12 @@ package uk.gov.hmcts.reform.sendletter.model.in.print;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.sendletter.model.Document;
 import uk.gov.hmcts.reform.sendletter.model.in.PrintRequest;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.io.Resources.getResource;
@@ -35,6 +38,32 @@ class PrintRequestTest {
             .contains(
                 tuple("1.pdf", 2),
                 tuple("2.pdf", 1)
+            );
+    }
+
+    @Test
+    void should_set_all_fields_when_intialised_with_values() {
+        List<Document> documents = List.of(
+            new Document("mypdf.pdf", 2),
+            new Document("1.pdf", 1)
+        );
+
+        PrintRequest printRequest = new PrintRequest(
+            documents,
+            Map.of("x", "y")
+        );
+
+        assertThat(printRequest.documents)
+            .as("documents list")
+            .extracting("fileName", "copies")
+            .contains(
+                tuple("mypdf.pdf", 2),
+                tuple("1.pdf", 1)
+            );
+
+        assertThat(printRequest.additionalData)
+            .containsExactly(
+                entry("x", "y")
             );
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sendletter/model/out/PrintResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/model/out/PrintResponseTest.java
@@ -10,6 +10,7 @@ import uk.gov.hmcts.reform.sendletter.model.Document;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.google.common.base.Charsets.UTF_8;
@@ -33,12 +34,14 @@ class PrintResponseTest {
             .isEqualTo(UUID.fromString("33dffc2f-94e0-4584-a973-cc56849ecc0b"));
         assertThat(printJob.createdAt)
             .isEqualTo(LocalDateTime.parse("2021-04-07T10:03:00"));
-        assertThat(printJob.printedAt).isNull();
-        assertThat(printJob.sentToPrintAt).isNull();
+        assertThat(printJob.printedAt)
+            .isEqualTo(LocalDateTime.parse("2021-04-08T11:03:00"));
+        assertThat(printJob.sentToPrintAt)
+            .isEqualTo(LocalDateTime.parse("2021-04-09T12:03:00"));
         assertThat(printJob.service)
             .isEqualTo("sscs");
         assertThat(printJob.printStatus)
-            .isEqualTo(PrintStatus.NEW);
+            .isEqualTo(PrintStatus.PROCESSED);
 
         List<Document> documents = printJob.documents;
         assertThat(documents)
@@ -62,7 +65,70 @@ class PrintResponseTest {
             .isEqualTo("?sas=sadas56tfuvydasd");
         assertThat(printUploadInfo.uploadToPath)
             .isEqualTo("/sscs/33dffc2f-94e0-4584-a973-cc56849ecc0b/file1-c10.pdf");
+    }
 
+    @Test
+    void should_set_all_fields_when_intialised_with_values() {
 
+        List<Document> documents = List.of(
+            new Document("mypdf.pdf", 2),
+            new Document("1.pdf", 1)
+        );
+
+        Map<String, String> additionalData = Map.of("x", "y");
+        UUID uuid = UUID.randomUUID();
+        LocalDateTime createAt = LocalDateTime.now();
+        LocalDateTime printedAt = createAt.plusDays(1);
+        LocalDateTime sentToPrint = createAt.plusDays(2);
+
+        PrintResponse response = new PrintResponse(
+            new PrintJob(
+                uuid,
+                createAt,
+                printedAt,
+                sentToPrint,
+                "sscs",
+                PrintStatus.ZIPPED,
+                documents,
+                additionalData
+            ),
+            new PrintUploadInfo(
+                "url",
+                "token",
+                "path"
+            )
+        );
+
+        PrintJob printJob = response.printJob;
+        assertThat(printJob).isNotNull();
+        assertThat(printJob.id)
+            .isEqualTo(uuid);
+        assertThat(printJob.createdAt)
+            .isEqualTo(createAt);
+        assertThat(printJob.printedAt)
+            .isEqualTo(printedAt);
+        assertThat(printJob.sentToPrintAt)
+            .isEqualTo(sentToPrint);
+        assertThat(printJob.service)
+            .isEqualTo("sscs");
+        assertThat(printJob.printStatus)
+            .isEqualTo(PrintStatus.ZIPPED);
+
+        assertThat(printJob.documents)
+            .as("documents list")
+            .extracting("fileName", "copies")
+            .contains(
+                tuple("mypdf.pdf", 2),
+                tuple("1.pdf", 1)
+            );
+
+        PrintUploadInfo printUploadInfo = response.printUploadInfo;
+        assertThat(printUploadInfo).isNotNull();
+        assertThat(printUploadInfo.uploadToContainer)
+            .isEqualTo("url");
+        assertThat(printUploadInfo.sasToken)
+            .isEqualTo("token");
+        assertThat(printUploadInfo.uploadToPath)
+            .isEqualTo("path");
     }
 }
