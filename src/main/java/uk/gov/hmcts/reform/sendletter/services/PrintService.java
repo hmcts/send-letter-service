cb7ea23b757f485ef0300f4f79b77e9ab00c8f70
rishikrsharma <rishikrsharma@users.noreply.github.com>
2021-08-06 09:45:49 +0100

    DTSBPS-697: printrequest log (#1220)
    * DTSBPS-697: printrequest log
    
    * added test for exception
    
    * added test for exception

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/PrintService.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/PrintService.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.sendletter.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sendletter.entity.Print;
@@ -24,6 +27,7 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 public class PrintService {
+    private static final Logger LOG = LoggerFactory.getLogger(PrintService.class);
     private final PrintRepository repository;
     private final ObjectMapper mapper;
     private final SasTokenGeneratorService sasTokenGeneratorService;
@@ -40,6 +44,12 @@ public class PrintService {
 
     @Transactional
     public PrintResponse save(String id, String service, PrintRequest request) {
+        try {
+            var req =  mapper.writeValueAsString(request);
+            LOG.info("PrintService PrintRequest {}", req);
+        } catch (JsonProcessingException jpe) {
+            LOG.info("PrintService JsonProcessingException {}", jpe.getMessage(), jpe);
+        }
         String checksum = LetterChecksumGenerator.generateChecksum(request);
         var printRequest = new Print(
             UUID.fromString(id),
