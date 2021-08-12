ae3abb87cc3dd49af3b143401265594289970791
rishisharma <rishi.sharma@hmcts.net>
2021-08-12 21:39:53 +0100

    DTSBPS-709: Azure Provider block

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.72.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "=2.72.0"
   features {}
 }
 
