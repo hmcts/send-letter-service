f1dc916e26273dbb5d220489d3ced84a337a3034
rishisharma <rishi.sharma@hmcts.net>
2021-08-12 16:24:20 +0100

    DTSBPS-709: removed outout

--- infrastructure/appinsights.tf
+++ infrastructure/appinsights.tf
@@ -12,7 +12,3 @@ resource "azurerm_application_insights" "appinsights" {
     ]
   }
 }
-
-output "appInsightsInstrumentationKey" {
-  value = azurerm_application_insights.appinsights.instrumentation_key
-}
