46aae6fa71396bf50385e3da655c7dcd3072a37b
rishisharma <rishi.sharma@hmcts.net>
2021-10-27 16:43:41 +0100

    fix failing config

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/config/SftpConfig.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/config/SftpConfig.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.sendletter.config;
 
 import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.transport.verification.HostKeyVerifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.reform.sendletter.helper.FakeFtpAvailabilityChecker;
 import uk.gov.hmcts.reform.sendletter.services.ftp.IFtpAvailabilityChecker;
 
+import java.security.PublicKey;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Supplier;
 
 @Configuration
@@ -19,7 +23,17 @@ public class SftpConfig {
         // host name and key for local testing.
         return () -> {
             SSHClient client = new SSHClient();
-            client.addHostKeyVerifier((a, b, c) -> true);
+            client.addHostKeyVerifier(new HostKeyVerifier() {
+                @Override
+                public boolean verify(String hostname, int port, PublicKey key) {
+                    return true;
+                }
+
+                @Override
+                public List<String> findExistingAlgorithms(String hostname, int port) {
+                    return Collections.emptyList();
+                }
+            });
             return client;
         };
     }
