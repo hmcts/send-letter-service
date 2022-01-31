4af865377e34ba31ae92b079596fa5a718102d13
mdayican <mustafa.dayican@hmcts.net>
2022-01-31 10:32:15 +0000

    DTSBPS-857 add app_gw subnet

--- infrastructure/storage-account.tf
+++ infrastructure/storage-account.tf
@@ -25,6 +25,7 @@ locals {
     data.azurerm_subnet.jenkins_subnet.id,
     data.azurerm_subnet.jenkins_aks_00.id,
     data.azurerm_subnet.jenkins_aks_01.id,
+    data.azurerm_subnet.aks_app_gw.id,
     data.azurerm_subnet.app_aks_00_subnet.id,
     data.azurerm_subnet.app_aks_01_subnet.id
   ]
