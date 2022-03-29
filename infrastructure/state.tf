468dd6ca44b46c46492e26757795037439f3a1d3
mdayican <mustafa.dayican@hmcts.net>
2022-03-29 13:58:27 +0100

    fix common_tags

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.72.0"
+      version = "~>2.99.0"
     }
     random = {
       source = "hashicorp/random"
