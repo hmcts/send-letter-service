b298f4605a2af7f131f40c220db4d222ab53635f
mdayican <mustafa.dayican@hmcts.net>
2022-03-30 08:56:06 +0100

    upgrade azurerm to fix master pipeline

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.72.0"
+      version = "~>3.0.2"
     }
     random = {
       source = "hashicorp/random"
