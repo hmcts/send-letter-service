d13e51d178d0f6b4e9cb925c397da474e97fe54f
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2022-08-03 17:09:05 +0100

    Write FTP files summary to csv files

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/FtpFileSummaryService.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/FtpFileSummaryService.java
@@ -5,32 +5,55 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sendletter.services.ftp.FileInfo;
 import uk.gov.hmcts.reform.sendletter.services.ftp.FtpClient;
+import uk.gov.hmcts.reform.sendletter.services.ftp.IFtpAvailabilityChecker;
 import uk.gov.hmcts.reform.sendletter.services.ftp.ServiceFolderMapping;
+import uk.gov.hmcts.reform.sendletter.util.CsvWriter;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Collections.emptyMap;
 import static uk.gov.hmcts.reform.sendletter.util.TimeZones.EUROPE_LONDON;
 
 @Service
-public class FtpFileService {
-    private static final Logger log = LoggerFactory.getLogger(FtpFileService.class);
+public class FtpFileSummaryService {
+    private static final Logger log = LoggerFactory.getLogger(FtpFileSummaryService.class);
 
+    private final IFtpAvailabilityChecker ftpAvailabilityChecker;
     private final FtpClient ftp;
     private final ServiceFolderMapping serviceFolderMapping;
 
     // region constructor
-    public FtpFileService(FtpClient ftp,
-                          ServiceFolderMapping serviceFolderMapping) {
+    public FtpFileSummaryService(IFtpAvailabilityChecker ftpAvailabilityChecker,
+                                 FtpClient ftp,
+                                 ServiceFolderMapping serviceFolderMapping) {
+        this.ftpAvailabilityChecker = ftpAvailabilityChecker;
         this.ftp = ftp;
         this.serviceFolderMapping = serviceFolderMapping;
     }
     // endregion
 
-    public Map<String, List<FileInfo>> listLettersForAllServices() {
+    public Map<String, File> getFtpFileUploadSummaryFiles() {
+        if (!ftpAvailabilityChecker.isFtpAvailable(LocalTime.now(ZoneId.of(EUROPE_LONDON)))) {
+            log.info("Not generating Bulk Print FTP Uploaded Letters Summary due to FTP downtime window");
+            return emptyMap();
+        }
+
+        try {
+            Map<String, List<FileInfo>> ftpLettersForServices = listLettersForAllServices();
+            return CsvWriter.writeFtpLettersToCsvFiles(ftpLettersForServices);
+        } catch (IOException exception) {
+            log.error("Unable to generate report", exception);
+        }
+        return emptyMap();
+    }
+
+    private Map<String, List<FileInfo>> listLettersForAllServices() {
         Map<String, List<FileInfo>> ftpUploadedLetters = new HashMap<>();
         log.info("Started listing uploaded letters to FTP server at {}", LocalTime.now(ZoneId.of(EUROPE_LONDON)));
         serviceFolderMapping
@@ -44,8 +67,8 @@ public class FtpFileService {
 
                         return null;
                     });
-                } catch (Exception e) {
-                    log.error("Error listing files from FTP folder {}", folder, e);
+                } catch (Exception exception) {
+                    log.error("Error listing files from FTP folder {}", folder, exception);
                 }
             });
 
