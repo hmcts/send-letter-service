f0af06a14bdc91c13ba43d055ee815c89222223e
Imran Ali <101260174+justiceia@users.noreply.github.com>
2023-03-08 10:46:15 +0000

    DTSBPS-1322: repository layer (#2154)
    * repository later
    
    * checkstyleMain issues fixed
    
    * checkstyleTest issues fixed
    
    * checkstyleIntegrationTest issues fixed
    
    * sonarcube coverage issues fixed
    
    * renamed findCreatedAfter to findOneCreatedAfter

---
+++ src/main/java/uk/gov/hmcts/reform/sendletter/entity/DocumentRepository.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.sendletter.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@SuppressWarnings("checkstyle:LineLength")
+public interface DocumentRepository extends JpaRepository<Document, UUID> {
+
+    @Query(value = "select * from documents d "
+        + " WHERE d.checksum = :checkSum "
+        + " AND d.created_at >= :createdAfter limit 1", nativeQuery = true)
+    Optional<Document> findOneCreatedAfter(@Param("checkSum") String checkSum, @Param("createdAfter") LocalDateTime createdAfter);
+
+}
