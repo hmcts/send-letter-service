b330db07a613459f9c200d62ebd6d7f70fb5cc91
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2023-03-14 20:20:00 +0000

    Fix test

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/LetterService.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/LetterService.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.sendletter.entity.DuplicateLetter;
 import uk.gov.hmcts.reform.sendletter.entity.ExceptionLetter;
