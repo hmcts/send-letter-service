7a911401391aa3e75a453c7c78cdea6177b3e334
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2023-03-14 15:41:54 +0000

    Fix test

--- src/functionalTest/java/uk/gov/hmcts/reform/sendletter/ProcessMessageWithDocumentCountTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sendletter/ProcessMessageWithDocumentCountTest.java
@@ -18,7 +18,7 @@ public class ProcessMessageWithDocumentCountTest extends FunctionalTestSuite {
     void should_send_letter_and_upload_file_on_sftp_server() throws Exception {
         String letterId = sendPrintLetterRequest(
             signIn(),
-            sampleIndexedPdfLetterRequestJson("letter-with-document-count-1.json", 11, 12)
+            sampleIndexedPdfLetterRequestJson("letter-with-document-count.json", 11, 12)
         );
 
         String status = verifyLetterUploaded(letterId);
