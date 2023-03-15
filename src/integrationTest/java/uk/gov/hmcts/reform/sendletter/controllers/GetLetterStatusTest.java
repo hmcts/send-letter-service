50a15aebdf4f6d762e98d7e8361b9dbc6576850d
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2023-03-15 15:36:42 +0000

    Reverted integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/controllers/GetLetterStatusTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/controllers/GetLetterStatusTest.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.sendletter.controllers;
 
 import com.google.common.io.Resources;
 import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
-import org.apache.http.HttpStatus;
 import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.mock.web.MockFilterConfig;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -125,45 +122,6 @@ class GetLetterStatusTest {
                 .andExpect(jsonPath("$.sent_to_print_at").isEmpty())
                 .andExpect(jsonPath("$.printed_at").isEmpty())
                 .andExpect(jsonPath("$.additional_data").doesNotHaveJsonPath());
-    }
-
-    @Test
-    void should_return_400_when_same_valid_json_is_sent_twice() throws Exception {
-        // given
-        given(tokenValidator.getServiceName("auth-header-value")).willReturn("some_service_name");
-
-        String json = Resources.toString(getResource("letter-with-pdf.json"), UTF_8);
-        MvcResult result = mvc
-            .perform(
-                post("/letters")
-                    .header("ServiceAuthorization", "auth-header-value")
-                    .contentType(MediaTypes.LETTER_V2)
-                    .content(json)
-            ).andReturn();
-
-        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
-        String letterId = jsonObject.getString("letter_id");
-        getLetterStatus(letterId)
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.id").isNotEmpty())
-            .andExpect(jsonPath("$.status").value(LetterStatus.Created.name()))
-            .andExpect(jsonPath("$.checksum").isNotEmpty())
-            .andExpect(jsonPath("$.created_at").isNotEmpty())
-            .andExpect(jsonPath("$.sent_to_print_at").isEmpty())
-            .andExpect(jsonPath("$.printed_at").isEmpty())
-            .andExpect(jsonPath("$.additional_data").doesNotHaveJsonPath());
-
-        String jsonDuplicate = Resources.toString(getResource("letter-with-pdf-duplicate.json"), UTF_8);
-        MvcResult resultDuplicate = mvc
-            .perform(
-                post("/letters")
-                    .header("ServiceAuthorization", "auth-header-value")
-                    .contentType(MediaTypes.LETTER_V2)
-                    .content(jsonDuplicate)
-            ).andReturn();
-
-        MockHttpServletResponse responseDuplicate = resultDuplicate.getResponse();
-        Assert.assertEquals(responseDuplicate.getStatus(), HttpStatus.SC_BAD_REQUEST);
     }
 
     @Test
