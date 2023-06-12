f7e18990e6f5fd7781afa7fa02b2238608076f82
alivenichoppa <97441560+alivenichoppa@users.noreply.github.com>
2023-06-12 14:26:44 +0100

    Update ProcessMessageWithDocumentCountTest.java

--- src/functionalTest/java/uk/gov/hmcts/reform/sendletter/ProcessMessageWithDocumentCountTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sendletter/ProcessMessageWithDocumentCountTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.sendletter;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -15,7 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @ExtendWith(SpringExtension.class)
-@Disabled
 public class ProcessMessageWithDocumentCountTest extends FunctionalTestSuite {
     private static final Logger logger = LoggerFactory.getLogger(ProcessMessageWithDocumentCountTest.class);
 
