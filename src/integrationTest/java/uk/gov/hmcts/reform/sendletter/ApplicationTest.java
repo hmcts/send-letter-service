ecb1662ca5cb17124e506639841f90711d55d5d6
Ruth Bovell <ruthb111@outlook.com>
2024-03-06 13:33:18 +0000

    - wip to fix integration test failures

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/ApplicationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/ApplicationTest.java
@@ -3,12 +3,10 @@ package uk.gov.hmcts.reform.sendletter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.io.FileOutputStream;
 
