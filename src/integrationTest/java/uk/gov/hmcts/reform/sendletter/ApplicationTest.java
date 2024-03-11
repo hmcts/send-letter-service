ad9845af25c33151299420dbcdd1833f5a8c573d
Ruth Bovell <ruthb111@outlook.com>
2024-03-11 15:40:12 +0000

    - fix ApplicationTest by upgrading springdocs version - update bouncycastle to jdk 1.8 and beyond

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/ApplicationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/ApplicationTest.java
@@ -1,39 +1,37 @@
 package uk.gov.hmcts.reform.sendletter;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.io.FileOutputStream;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.FileOutputStream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ComponentScan(basePackages = "...", lazyInit = true)
 @ContextConfiguration
 @SpringBootTest
 @AutoConfigureMockMvc
-@WebMvcTest(Application.class)
 class ApplicationTest {
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    void should_generate_swagger_docs() throws Exception {
-//        byte[] specs = mvc.perform(get("/v3/api-docs"))
-//            .andExpect(status().isOk())
-//            .andReturn()
-//            .getResponse()
-//            .getContentAsByteArray();
-//
-//        FileOutputStream outputStream = new FileOutputStream("/tmp/swagger-specs.json");
-//        outputStream.write(specs);
-//        outputStream.close();
-//    }
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void should_generate_swagger_docs() throws Exception {
+        byte[] specs = mvc.perform(get("/v3/api-docs"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        FileOutputStream outputStream = new FileOutputStream("/tmp/swagger-specs.json");
+        outputStream.write(specs);
+        outputStream.close();
+    }
 }
