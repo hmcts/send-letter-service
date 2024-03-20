55ff4319ede0021a62f4976bfa478378705d5606
Ruth Bovell <ruthb111@outlook.com>
2024-03-20 11:20:02 +0000

    - wip to fix functional tests, changes to RestTemplateConfiguration

--- src/main/java/uk/gov/hmcts/reform/sendletter/config/RestTemplateConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/config/RestTemplateConfiguration.java
@@ -1,25 +1,22 @@
 package uk.gov.hmcts.reform.sendletter.config;
 
 import feign.Client;
-import feign.hc5.ApacheHttp5Client;
-import org.apache.hc.client5.http.config.ConnectionConfig;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import feign.httpclient.ApacheHttpClient;
+import org.apache.hc.core5.util.Timeout;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RestTemplateConfiguration {
 
     @Bean
     public Client getFeignHttpClient() {
-        return new ApacheHttp5Client(getHttpClient());
+        return new ApacheHttpClient(getHttpClient());
     }
 
     @Bean
@@ -29,25 +26,33 @@ public class RestTemplateConfiguration {
 
     @Bean
     public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
-        return new HttpComponentsClientHttpRequestFactory(getHttpClient());
+        return new HttpComponentsClientHttpRequestFactory(getHttp5Client());
     }
 
-    private CloseableHttpClient getHttpClient() {
-        ConnectionConfig connectionConfig = ConnectionConfig.custom()
-            .setConnectTimeout(30000, TimeUnit.MILLISECONDS)
-            .setSocketTimeout(60000, TimeUnit.MILLISECONDS)
+    private org.apache.hc.client5.http.classic.HttpClient getHttp5Client() {
+        org.apache.hc.client5.http.config.RequestConfig config =
+            org.apache.hc.client5.http.config.RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(30))
+                .build();
+
+        return org.apache.hc.client5.http.impl.classic.HttpClientBuilder
+            .create()
+            .useSystemProperties()
+            .setDefaultRequestConfig(config)
+            .build();
+    }
+
+    private HttpClient getHttpClient() {
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(30000)
+            .setConnectionRequestTimeout(30000)
+            .setSocketTimeout(60000)
             .build();
 
-        RequestConfig requestConfig = RequestConfig.custom()
-            .setConnectionRequestTimeout(30000, TimeUnit.MILLISECONDS)
-            .build();
-
-        BasicHttpClientConnectionManager cm = new BasicHttpClientConnectionManager();
-        cm.setConnectionConfig(connectionConfig);
-
-        return HttpClientBuilder.create()
-            .setDefaultRequestConfig(requestConfig)
-            .setConnectionManager(cm)
+        return HttpClientBuilder
+            .create()
+            .useSystemProperties()
+            .setDefaultRequestConfig(config)
             .build();
     }
 }
