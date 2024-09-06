2eddb87c42d65961bd642e5d48d348c157fdcd69
Ruth Bovell <ruthb111@outlook.com>
2024-09-06 11:55:24 +0100

    - adds some warning logs

--- src/main/java/uk/gov/hmcts/reform/sendletter/logging/AppInsights.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/logging/AppInsights.java
@@ -189,10 +189,18 @@ public class AppInsights {
         }
 
         String dependencyName = dependency.name();
+
         if (!dependencyName.startsWith("GET /health")
             || !dependencyName.contains("shedlock")
             || !dependencyName.equalsIgnoreCase("send_letter")) {
+            log.warn(
+                "This is a dependency we want to keep tracking: " + dependencyName
+            );
             telemetryClient.trackDependency(dependencyTelemetry);
+        } else {
+            log.warn(
+                "This is a dependency we DO NOT want to keep tracking: " + dependencyName
+            );
         }
     }
 
