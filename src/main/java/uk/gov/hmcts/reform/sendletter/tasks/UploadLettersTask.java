d4b090a50bad862d7c90dc4086401320081d06cd
Ruth Bovell <ruthb111@outlook.com>
2024-10-24 10:40:49 +0100

    - changes key in appinsights json to have a dot - removes log added for debugging

--- src/main/java/uk/gov/hmcts/reform/sendletter/tasks/UploadLettersTask.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/tasks/UploadLettersTask.java
@@ -158,7 +158,6 @@ public class UploadLettersTask {
                     grabbedServiceFolder = serviceFolder.get() + INTERNATIONAL_FOLDER;
                 }
             }
-            logger.info("Service folder found for letter service: {}", grabbedServiceFolder);
             uploadLetter(letter, grabbedServiceFolder, sftpClient);
             letter.setStatus(LetterStatus.Uploaded);
             letter.setSentToPrintAt(now());
