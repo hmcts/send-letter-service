dec41e66d93ff43511d43c00e70c03aae69d0bc7
Ruth Bovell <ruthb111@outlook.com>
2024-10-29 11:06:07 +0000

    - updates test to reflect reduction in logs

--- src/test/java/uk/gov/hmcts/reform/sendletter/tasks/UploadLettersTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/tasks/UploadLettersTaskTest.java
@@ -139,7 +139,7 @@ class UploadLettersTaskTest {
         ).containsExactlyInAnyOrder(false, true);
 
         List<LogEvent> logEvents = uploadLettersTaskLogCaptor.getLogEvents();
-        assertTrue(logEvents.get(4).getMessage().contains("folder: some_folder"),ASSERTION_MESSAGE);
+        assertTrue(logEvents.get(3).getMessage().contains("folder: some_folder"),ASSERTION_MESSAGE);
     }
 
     @Test
