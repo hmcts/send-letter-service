cb955e6be3d0f5b14d4e89739681f7183c766556
Dawid Strozak <dawid.strozak@hmcts.net>
2026-04-27 17:53:26 +0100

    Grant new jenkins mi per environment on the KV

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,12 +14,18 @@ module "send-letter-key-vault" {
   tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
   resource_group_name = azurerm_resource_group.rg.name
+  jenkins_object_id   = data.azurerm_user_assigned_identity.jenkins.principal_id
 
   # dcd_cc-dev group object ID
   product_group_object_id              = "38f9dea6-e861-4a50-9e73-21e64f563537"
   common_tags                          = var.common_tags
   create_managed_identity              = true
   additional_managed_identities_access = ["rpe-shared", "bulk-scan"]
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 data "azurerm_key_vault_secret" "smtp_username" {
