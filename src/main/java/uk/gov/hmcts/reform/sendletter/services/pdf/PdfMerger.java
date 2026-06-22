471faecdbac53e815c8ad1e3fa494ed793e7fc09
Jon <hurricanepilot@gmail.com>
2026-06-22 10:54:24 +0100

    Fact 2683 pdf merge issue investigation. (DO NOT MERGE) (#2777)
    * Add OPTIMIZE_RESOURCES_MODE fallback for pdf merge
    
    * Remove unnecessary method param
    
    * Additional logging for merge code
    
    * Add test for provided file
    
    ---------
    
    Co-authored-by: Adam Stevenson <addstevenson@hotmail.com>

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/pdf/PdfMerger.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/pdf/PdfMerger.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sendletter.services.pdf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.io.RandomAccessRead;
 import org.apache.pdfbox.io.RandomAccessReadBuffer;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
@@ -15,6 +16,7 @@ import static org.apache.pdfbox.io.MemoryUsageSetting.setupMainMemoryOnly;
 /**
  * Utility class to merge PDF documents.
  */
+@Slf4j
 public final class PdfMerger {
 
     /**
@@ -26,15 +28,31 @@ public final class PdfMerger {
     /**
      * Merges a list of PDF documents into a single PDF document.
      *
-     * @param documents       The list of PDF documents
-     * @param loggingContext  The logging context
+     * @param documents      The list of PDF documents
+     * @param loggingContext The logging context
      * @return The merged PDF document
      */
     public static byte[] mergeDocuments(List<byte[]> documents, String loggingContext) {
         if (documents.size() == 1) {
-            return documents.get(0);
+            return documents.getFirst();
         }
 
+        try {
+            log.info("Merging {} PDFs using default mode", documents.size());
+            return mergeDocuments(documents, false);
+        } catch (IOException e) {
+            try {
+                log.warn("Error while merging documents, attempting optimised merge", e);
+                return mergeDocuments(documents, true);
+            } catch (IOException e1) {
+                log.warn("Optimised merge failed, throwing original exception");
+                throw new PdfMergeException("Exception occurred while merging PDF files." + loggingContext, e);
+            }
+        }
+    }
+
+    private static byte[] mergeDocuments(List<byte[]> documents, boolean optimizeResourcesMode)
+        throws IOException {
         ByteArrayOutputStream docOutputStream = new ByteArrayOutputStream();
 
         List<RandomAccessRead> inputStreams = documents.stream()
@@ -45,11 +63,14 @@ public final class PdfMerger {
         pdfMergerUtility.addSources(inputStreams);
         pdfMergerUtility.setDestinationStream(docOutputStream);
 
-        try {
-            pdfMergerUtility.mergeDocuments(setupMainMemoryOnly().streamCache);
-            return docOutputStream.toByteArray();
-        } catch (IOException e) {
-            throw new PdfMergeException("Exception occurred while merging PDF files." + loggingContext, e);
+        if (optimizeResourcesMode) {
+            // FACT-2863 - adding mode switch to prevent documents with unsupported structures from
+            // causing merge failure. This mode effectively reduces supported content to "Page
+            // content and resources".
+            pdfMergerUtility.setDocumentMergeMode(PDFMergerUtility.DocumentMergeMode.OPTIMIZE_RESOURCES_MODE);
         }
+
+        pdfMergerUtility.mergeDocuments(setupMainMemoryOnly().streamCache);
+        return docOutputStream.toByteArray();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sendletter/services/PdfMergerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/services/PdfMergerTest.java
@@ -1,12 +1,19 @@
 package uk.gov.hmcts.reform.sendletter.services;
 
 import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSInteger;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.common.PDNumberTreeNode;
+import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.PDStructureTreeRoot;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.sendletter.exception.PdfMergeException;
 import uk.gov.hmcts.reform.sendletter.services.pdf.PdfMerger;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
@@ -40,6 +47,51 @@ class PdfMergerTest {
     }
 
     @Test
+    void should_still_create_a_merged_pdf_when_source_pdfs_contain_unsupported_structures() throws Exception {
+        //given
+        byte[] test1Pdf = createStructTreePdf();
+        byte[] test2Pdf = loadResource("test2.pdf");
+
+        //when
+        byte[] actualMergedPdf = PdfMerger.mergeDocuments(asList(test1Pdf, test2Pdf), "test_service");
+
+        // then
+        try (
+            // the struct tree PDF doesn't have a page we'll just check that page 0 is
+            // the same as page 0 of the second PDF
+            InputStream actualPdfPage1 = getPdfPageContents(actualMergedPdf, 0);
+            InputStream expectedPdfPage1 = getPdfPageContents(test2Pdf, 0)
+        ) {
+            assertThat(actualPdfPage1).hasSameContentAs(expectedPdfPage1);
+        }
+    }
+
+    @Test
+    void should_still_create_a_merged_pdf_when_source_pdfs_are_structurally_broken() throws Exception {
+
+        // this test is essentially the same as the above test, but using a provided PDF
+
+        //given
+        byte[] test1Pdf = loadResource("fact-2683_bad_doc.pdf");
+        byte[] test2Pdf = loadResource("test2.pdf");
+
+        //when
+        byte[] actualMergedPdf = PdfMerger.mergeDocuments(asList(test1Pdf, test2Pdf), "test_service");
+
+        // then
+        try (
+            InputStream actualPdfPage1 = getPdfPageContents(actualMergedPdf, 0);
+            InputStream actualPdfPage2 = getPdfPageContents(actualMergedPdf, 1);
+
+            InputStream expectedPdfPage1 = getPdfPageContents(test1Pdf, 0);
+            InputStream expectedPdfPage2 = getPdfPageContents(test2Pdf, 0)
+        ) {
+            assertThat(actualPdfPage1).hasSameContentAs(expectedPdfPage1);
+            assertThat(actualPdfPage2).hasSameContentAs(expectedPdfPage2);
+        }
+    }
+
+    @Test
     void should_return_a_merged_pdf_same_as_original_pdf_when_single_pdf_is_sent() throws Exception {
         //given
         byte[] testPdf = loadResource("test1.pdf");
@@ -63,5 +115,22 @@ class PdfMergerTest {
 
     private static void merge() {
         PdfMerger.mergeDocuments(asList("test1".getBytes(), "test2".getBytes()), "test_service");
+    }
+
+    // FACT-2683 - creates a PDF with a struct tree containing a COSArray/COSInteger to test
+    // that the merge code can handle this without throwing an exception
+    private byte[] createStructTreePdf() {
+        try (PDDocument document = new PDDocument();
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            PDNumberTreeNode pdNumberTreeNode = new PDNumberTreeNode(COSArray.class);
+            pdNumberTreeNode.setNumbers(Map.of(1, COSInteger.get(100)));
+            PDStructureTreeRoot structureTreeRoot = new PDStructureTreeRoot();
+            structureTreeRoot.setParentTree(pdNumberTreeNode);
+            document.getDocumentCatalog().setStructureTreeRoot(structureTreeRoot);
+            document.save(outputStream);
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
